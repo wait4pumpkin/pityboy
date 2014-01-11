05f1d96228a30b13a7a85e4e6dcdff6550def550
Pumpkin <wait4pumpkin@gmail.com>
2014-01-12 00:43:46 +0800

    set haml option

--- config/boot.rb
+++ config/boot.rb
@@ -7,6 +7,7 @@ require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
 
+require 'haml/template/options'
 Haml::Template.options[:format] = :html5
 
 ##
